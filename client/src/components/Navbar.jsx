34089ecc44ead88df16124c0e2099a72f18b115f
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-11-17 16:12:01 +0530

    slide animation

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -203,7 +203,7 @@
 // }
 
 import { useEffect, useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 import { debounce } from "time-loom";
 
 import { useSelector, useDispatch } from "react-redux";
@@ -211,9 +211,9 @@ import { logout } from "../redux/actions/userActions";
 
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
-import { Heart, Menu, ShoppingCart, User } from "lucide-react";
+import { Heart, Menu, ShoppingCart, User, X } from "lucide-react";
+import 'animate.css'; // Import Animate.css for animations
 import SageLogo from "../assets/sage-logo.png";
-import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
   const { user } = useSelector((state) => state.user);
@@ -335,17 +335,13 @@ const Navbar = () => {
   };
 
   const [showSideNavbar, setShowSideNavbar] = useState(false);
-  const toggleSideNavbar = () => {
-    // console.log("Hamburger clicked");
-    setShowSideNavbar(!showSideNavbar);
-  };
 
   return (
     <header className="border-b">
       <div className="container mx-auto px-4 py-4">
         <div className="flex items-center justify-between gap-4">
           <a href="/" className="flex-shrink-0">
-            <img src={SageLogo} width={100} height={100} />
+            <img src={SageLogo} width={100} height={100} alt="Logo" />
           </a>
           <div className="relative hidden flex-1 max-w-xl lg:block">
             <SearchBar
@@ -353,13 +349,6 @@ const Navbar = () => {
               search={search}
               setSearch={setSearch}
             />
-            {/* <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-500" />
-
-            <Input
-              className="w-full bg-gray-100 pl-10"
-              placeholder="Search for Products"
-              type="search"
-            /> */}
           </div>
           <div className="flex items-center gap-4">
             <Link to="/dashboard/wishlist" variant="ghost" size="icon">
@@ -373,139 +362,69 @@ const Navbar = () => {
             </Link>
             <Menu
               className="h-5 w-5 hidden max-lg:block cursor-pointer"
-              onClick={toggleSideNavbar}
+              onClick={()=> setShowSideNavbar(true)}
             />
           </div>
         </div>
         {showSideNavbar && (
-          <nav className="mt-4 py-3 rounded-md bg-[#166272] lg:hidden animate-slide-down">
-            <ul className="flex flex-col gap-4 px-4">
-              {categories.map((category) => (
-                <li key={category}>
-                  <Link
-                    className="text-sm font-medium text-white hover:text-gray-900"
-                    href={`/category/${category.toLowerCase()}`}
-                  >
-                    {category}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
+          <div
+          className={`fixed top-0 right-0 w-1/2 h-screen bg-[#166272] z-50 animate__animated ${
+            showSideNavbar ? 'animate__fadeInRight' : 'animate__fadeOutRight'
+          }`}
+          >
+            {/* Close button */}
+            <X
+              className="absolute top-4 left-4 h-6 w-6 text-white cursor-pointer"
+              onClick={() => setShowSideNavbar(false)} // Close sidebar on click
+            />
+
+            <div className="relative flex-1 flex justify-center items-center max-w-xl mx-auto mt-12 w-3/4">
+              <SearchBar
+                handleClick={handleClick}
+                search={search}
+                setSearch={setSearch}
+              />
+            </div>
+
+            <nav className="mt-4 py-3 rounded-md">
+              <ul className="flex flex-col gap-4 px-4">
+                {categories.map((category) => (
+                  <li key={category} className="border-b pb-3 border-white">
+                    <Link
+                      className="text-sm font-medium text-white hover:text-gray-900"
+                      href={`/category/${category.toLowerCase()}`}
+                    >
+                      {category}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
         )}
-        <nav className="mt-4 hidden lg:block py-3 rounded-md  bg-[#166272]">
-          <ul className="flex gap-8 px-4">
-            {categories.map((category) => (
-              <li key={category}>
-                <Link
-                  className="text-sm font-medium text-white hover:text-gray-900"
-                  href={`/category/${category.toLowerCase()}`}
-                >
-                  {category}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
+
+        {/* Conditional rendering for the nav only on "/" route */}
+        {window.location.pathname === "/" && (
+          <div className="w-full flex justify-center items-center">
+            <nav className="mt-4 hidden lg:block py-3 rounded-md w-fit bg-[#166272]">
+              <ul className="flex gap-8 px-4 justify-center items-center">
+                {categories.map((category) => (
+                  <li key={category}>
+                    <Link
+                      className="text-sm font-medium text-white hover:text-gray-900"
+                      href={`/category/${category.toLowerCase()}`}
+                    >
+                      {category}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+        )}
       </div>
     </header>
   );
 };
 
 export default Navbar;
-
-/*
-
-
-*/
-function HeartIcon(props) {
-  return (
-    <svg
-      width="28"
-      height="25"
-      viewBox="0 0 28 25"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M13.75 25L12.5924 23.9305C10.099 21.6014 8.03649 19.6077 6.4049 17.9494C4.77331 16.2912 3.48547 14.8287 2.54138 13.5619C1.59729 12.2952 0.937712 11.1492 0.562642 10.1238C0.187548 9.09837 0 8.06697 0 7.02956C0 5.04285 0.660076 3.37458 1.98023 2.02475C3.30038 0.674916 4.93197 0 6.875 0C8.21865 0 9.47907 0.351478 10.6562 1.05443C11.8334 1.75739 12.8647 2.77977 13.75 4.12159C14.6353 2.77977 15.6666 1.75739 16.8438 1.05443C18.0209 0.351478 19.2813 0 20.625 0C22.568 0 24.1996 0.674916 25.5198 2.02475C26.8399 3.37458 27.5 5.04285 27.5 7.02956C27.5 8.06697 27.3125 9.09837 26.9374 10.1238C26.5623 11.1492 25.9027 12.2952 24.9586 13.5619C24.0145 14.8287 22.7316 16.2912 21.1098 17.9494C19.488 19.6077 17.4206 21.6014 14.9076 23.9305L13.75 25ZM13.75 22.8851C16.1944 20.6261 18.206 18.6909 19.7847 17.0797C21.3634 15.4685 22.6111 14.0707 23.5278 12.886C24.4444 11.7014 25.081 10.652 25.4375 9.73774C25.794 8.8235 25.9722 7.92078 25.9722 7.02956C25.9722 5.46743 25.463 4.16566 24.4444 3.12425C23.4259 2.08283 22.1528 1.56212 20.625 1.56212C19.4087 1.56212 18.2863 1.9171 17.258 2.62706C16.2297 3.33702 15.3091 4.40598 14.4963 5.83395H13.0037C12.1713 4.38596 11.2458 3.312 10.2273 2.61207C9.20877 1.91211 8.09134 1.56212 6.875 1.56212C5.3668 1.56212 4.09856 2.08283 3.07026 3.12425C2.04194 4.16566 1.52778 5.46743 1.52778 7.02956C1.52778 7.92078 1.70602 8.8235 2.0625 9.73774C2.41898 10.652 3.05556 11.7014 3.97222 12.886C4.88889 14.0707 6.13657 15.4635 7.71528 17.0647C9.29398 18.6659 11.3056 20.606 13.75 22.8851Z"
-        fill="white"
-      />
-    </svg>
-  );
-}
-
-function MenuIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="white"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <line x1="4" x2="20" y1="12" y2="12" />
-      <line x1="4" x2="20" y1="6" y2="6" />
-      <line x1="4" x2="20" y1="18" y2="18" />
-    </svg>
-  );
-}
-
-function SearchIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <circle cx="11" cy="11" r="8" />
-      <path d="m21 21-4.3-4.3" />
-    </svg>
-  );
-}
-
-function ShoppingBagIcon(props) {
-  return (
-    <svg
-      width="23"
-      height="25"
-      viewBox="0 0 23 25"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M2.27162 25C1.64242 25 1.10651 24.7814 0.663891 24.3443C0.221297 23.9072 0 23.3779 0 22.7564V7.79913C0 7.1777 0.221297 6.6484 0.663891 6.21125C1.10651 5.77412 1.64242 5.55556 2.27162 5.55556H5.625C5.625 4.00819 6.17082 2.69543 7.26247 1.61726C8.35412 0.539087 9.6833 0 11.25 0C12.8167 0 14.1459 0.539087 15.2375 1.61726C16.3292 2.69543 16.875 4.00819 16.875 5.55556H20.2284C20.8576 5.55556 21.3935 5.77412 21.8361 6.21125C22.2787 6.6484 22.5 7.1777 22.5 7.79913V22.7564C22.5 23.3779 22.2787 23.9072 21.8361 24.3443C21.3935 24.7814 20.8576 25 20.2284 25H2.27162ZM2.27162 23.6111H20.2284C20.4447 23.6111 20.643 23.5221 20.8233 23.344C21.0036 23.1659 21.0938 22.9701 21.0938 22.7564V7.79913C21.0938 7.58547 21.0036 7.38961 20.8233 7.21153C20.643 7.03347 20.4447 6.94444 20.2284 6.94444H2.27162C2.05529 6.94444 1.85698 7.03347 1.67667 7.21153C1.49639 7.38961 1.40625 7.58547 1.40625 7.79913V22.7564C1.40625 22.9701 1.49639 23.1659 1.67667 23.344C1.85698 23.5221 2.05529 23.6111 2.27162 23.6111ZM11.25 13.8889C12.8167 13.8889 14.1459 13.3498 15.2375 12.2716C16.3292 11.1935 16.875 9.8807 16.875 8.33333H15.4688C15.4688 9.49074 15.0586 10.4745 14.2383 11.2847C13.418 12.0949 12.4219 12.5 11.25 12.5C10.0781 12.5 9.08203 12.0949 8.26172 11.2847C7.44141 10.4745 7.03125 9.49074 7.03125 8.33333H5.625C5.625 9.8807 6.17082 11.1935 7.26247 12.2716C8.35412 13.3498 9.6833 13.8889 11.25 13.8889ZM7.03125 5.55556H15.4688C15.4688 4.39815 15.0586 3.41435 14.2383 2.60417C13.418 1.79398 12.4219 1.38889 11.25 1.38889C10.0781 1.38889 9.08203 1.79398 8.26172 2.60417C7.44141 3.41435 7.03125 4.39815 7.03125 5.55556Z"
-        fill="white"
-      />
-    </svg>
-  );
-}
-
-function UserIcon(props) {
-  return (
-    <svg
-      width="28"
-      height="25"
-      viewBox="0 0 28 25"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M13.75 11.3372C12.1295 11.3372 10.7422 10.7822 9.58817 9.67208C8.43415 8.56198 7.85714 7.2275 7.85714 5.66864C7.85714 4.10975 8.43415 2.77525 9.58817 1.66515C10.7422 0.555049 12.1295 0 13.75 0C15.3705 0 16.7578 0.555049 17.9118 1.66515C19.0658 2.77525 19.6429 4.10975 19.6429 5.66864C19.6429 7.2275 19.0658 8.56198 17.9118 9.67208C16.7578 10.7822 15.3705 11.3372 13.75 11.3372ZM0 25V21.8895C0 21.1095 0.236091 20.3797 0.708273 19.7002C1.18045 19.0207 1.8157 18.4932 2.61402 18.1177C4.46749 17.2626 6.32222 16.6212 8.1782 16.1937C10.0342 15.7661 11.8915 15.5523 13.75 15.5523C15.6085 15.5523 17.4658 15.7661 19.3218 16.1937C21.1778 16.6212 23.0325 17.2626 24.886 18.1177C25.6843 18.4932 26.3195 19.0207 26.7917 19.7002C27.2639 20.3797 27.5 21.1095 27.5 21.8895V25H0ZM1.96429 23.1104H25.5357V21.8895C25.5357 21.4704 25.3953 21.0768 25.1145 20.7085C24.8337 20.3403 24.4453 20.0291 23.9492 19.7747C22.3324 19.0213 20.6655 18.4441 18.9483 18.0432C17.2311 17.6423 15.4984 17.4418 13.75 17.4418C12.0016 17.4418 10.2689 17.6423 8.55171 18.0432C6.83454 18.4441 5.16758 19.0213 3.55084 19.7747C3.05473 20.0291 2.66627 20.3403 2.38548 20.7085C2.10468 21.0768 1.96429 21.4704 1.96429 21.8895V23.1104ZM13.75 9.4477C14.8304 9.4477 15.7552 9.07767 16.5246 8.3376C17.2939 7.59754 17.6786 6.70788 17.6786 5.66864C17.6786 4.6294 17.2939 3.73974 16.5246 2.99968C15.7552 2.25961 14.8304 1.88958 13.75 1.88958C12.6696 1.88958 11.7448 2.25961 10.9754 2.99968C10.2061 3.73974 9.82143 4.6294 9.82143 5.66864C9.82143 6.70788 10.2061 7.59754 10.9754 8.3376C11.7448 9.07767 12.6696 9.4477 13.75 9.4477Z"
-        fill="white"
-      />
-    </svg>
-  );
-}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,23 +3,7 @@ export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      keyframes: {
-        'slide-down': {
-          '0%': {
-            transform: 'translateY(-50px)',
-            opacity: '0',
-          },
-          '100%': {
-            transform: 'translateY(0)',
-            opacity: '1',
-          },
-        }
-      },
-      animation: {
-        'slide-down': 'slide-down 0.3s ease-in-out',
-      },
-    },
+    extend: {},
   },
   plugins: [
     require('tailwind-scrollbar'),

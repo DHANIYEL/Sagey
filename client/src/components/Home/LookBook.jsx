2efb7e54b6d9362452439e9db85924a0b03998b5
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-11-25 01:14:17 +0530

    look book

--- client/src/components/Home/LookBook.jsx
+++ client/src/components/Home/LookBook.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react'
 import { Button } from "@/components/ui/button"
-import model1 from '../../assets/Sagey/model1.png'
-import model2 from '../../assets/Sagey/model2.png'
-import model3 from '../../assets/Sagey/product1.png'
-import model4 from '../../assets/Sagey/model1.png'
+import model1 from '../../../../server/public/products/1732472929425-Snapinsta.app_465312902_531366709670045_9154428972843883234_n_1080.jpg'
+import model2 from '../../../../server/public/products/1732473308785-Snapinsta.app_466090053_554177770557956_8071193005145551139_n_1080.jpg'
+import model3 from '../../../../server/public/products/1732472929424-Snapinsta.app_465128493_2269016723453875_5739826853210052398_n_1080.jpg'
+import model4 from '../../../../server/public/products/1732472816932-Snapinsta.app_465141660_1053882959850697_7929979004672098805_n_1080.jpg'
+import model5 from '../../../../server/public/products/1732304554345-product1.png'
+import model6 from '../../../../server/public/products/1732472663536-Snapinsta.app_466005848_1231451371306596_4627743878004052069_n_1080.jpg'
+import model7 from '../../../../server/public/products/1732473308780-Snapinsta.app_467114292_1276019447160269_9192971139706565781_n_1080.jpg'
+import model8 from '../../../../server/public/products/1732472578173-Snapinsta.app_466005131_867798948759603_334934234018594737_n_1080.jpg'
+import model9 from '../../../../server/public/products/1732472663526-Snapinsta.app_466092793_1242296907095569_8367549208719060317_n_1080.jpg'
+import model10 from '../../../../server/public/products/1732473308783-Snapinsta.app_467189993_8198649636901862_4509868729107994218_n_1080.jpg'
 
 const looks = [
   {
@@ -11,7 +17,7 @@ const looks = [
     title: 'Casual Stripes',
     images: [
         model1,
-        model2
+        model10
     ]
   },
   {
@@ -19,7 +25,7 @@ const looks = [
     title: 'Modern Minimal',
     images: [
         model2,
-        model3
+        model9
     ]
   },
   {
@@ -27,47 +33,47 @@ const looks = [
     title: 'Elegant Earth',
     images: [
         model3,
-        model4
+        model8
     ]
   },
   {
     id: 4,
     title: 'Casual Stripes',
     images: [
-        model1,
-        model2
+        model4,
+        model7
     ]
   },
   {
     id: 5,
     title: 'Modern Minimal',
     images: [
-        model2,
-        model3
+        model5,
+        model6
     ]
   },
   {
     id: 6,
     title: 'Elegant Earth',
     images: [
-        model3,
-        model4
+        model2,
+        model9
     ]
   },
   {
     id: 7,
     title: 'Casual Stripes',
     images: [
-        model1,
-        model2
+        model10,
+        model6
     ]
   },
   {
     id: 8,
     title: 'Modern Minimal',
     images: [
-        model2,
-        model3
+        model7,
+        model2
     ]
   },
   {

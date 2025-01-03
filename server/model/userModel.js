f478d022911f02f35e297fe2e4965dfb57ce8cb1
Matajar <es.matajar@gmail.com>
2025-01-03 14:39:26 +0530

    ne chnage

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -81,9 +81,6 @@ UserSchema.statics.signup = async function (
     throw Error("Email is not valid");
   }
 
-  // if (validator.isStrongPassword(password)) {
-  //   throw Error("Password is not strong enough");
-  // }
 
   // Checking if the email is already registered.
   const exists = await this.findOne({ email });
@@ -150,9 +147,7 @@ UserSchema.statics.changePassword = async function (
     throw Error("Password doesn't match");
   }
 
-  // if (!validator.isStrongPassword(password)) {
-  //   throw Error("Password is not strong enough");
-  // }
+
   const exists = await this.findOne({ _id });
   if (!exists) {
     throw Error("Cannot find email");
